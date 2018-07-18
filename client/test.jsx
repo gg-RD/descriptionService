6b38ca27d994644e1ca32a0eba1abad8fb59bea0
Disher Buttar <buttar.dilsher@gmail.com>
2018-07-18 13:41:20 -0700

    Initial commi. Added the set up files

---
+++ client/test.jsx
@@ -0,0 +1,22 @@
+class Description extends React.Component{
+	constructor(){
+		super();
+		this.state = {
+			shoe: {
+				name: 'Nike Air VaporMax Plus',
+				category: "Men's Shoes",
+				size: [6, 6.5, 7, 7.5, 8, 9, ]
+
+			}
+		}
+	}
+
+	render(){
+
+		
+	}
+
+
+}
+
+//making some changes to figure out the feature branch
