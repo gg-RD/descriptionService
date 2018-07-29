f8ade660d4ff2cf09a882be9276abb0451645e1d
Disher Buttar <buttar.dilsher@gmail.com>
2018-07-28 17:46:57 -0700

    Size table has hover effects

--- client/components/sizeTable.jsx
+++ client/components/sizeTable.jsx
@@ -1,30 +1,41 @@
 import React from 'react';
 
-const SizeTable = (props) => {
-  return (
-    <table>
-     <tbody>
-      <tr>
-        <td>7</td>
-        <td>7.5</td>
-        <td>8</td>
-        <td>8.5</td>      
+class SizeTable extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      backGroundBlack: false
+    }
+  }
+
+  render(){
+
+    return (
+      <table>
+       <tbody>
+        <tr>
+          <td>7</td>
+          <td>7.5</td>
+          <td>8</td>
+          <td>8.5</td>      
+        </tr>
+        <tr>
+          <td>9</td>
+          <td>9.5</td>
+          <td>10</td>
+          <td>10.5</td>
+        </tr>
+        <tr>
+          <td>11</td>
+          <td>11.5</td>
+          <td>12</td>
+          <td>12.5</td>       
       </tr>
-      <tr>
-        <td>9</td>
-        <td>9.5</td>
-        <td>10</td>
-        <td>10.5</td>
-      </tr>
-      <tr>
-        <td>11</td>
-        <td>11.5</td>
-        <td>12</td>
-        <td>12.5</td>       
-    </tr>
-    </tbody>
-    </table>
-    )
+      </tbody>
+     </table>
+      )
+  }
 
 }
 
